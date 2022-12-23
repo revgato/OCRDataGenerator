3cca1afd7d5a010166434b5dd4c589ebb8c80381
revgato <locchoihay147@gmail.com>
2022-12-23 07:14:27 +0000

    first commit

---
+++ img_generate.py
@@ -0,0 +1,51 @@
+import argparse
+import random
+import os
+from PIL import Image, ImageDraw, ImageFont
+import preprocessing
+
+A4_size = (1240, 1754)    # (A4 paper (150 PPI)
+
+# parse the input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--font", required=True, help="folder containing font")
+parser.add_argument("--background", required=True, help="folder containing background image")
+parser.add_argument("--color", required=True, help="font color")
+args = parser.parse_args()
+
+# read the dictionary file
+with open("dict.txt") as f:
+    dictionary = f.read().splitlines()
+
+PATH = os.getcwd()
+
+# create the image with the specified background
+background_path = os.path.join(PATH, args.background, random.choice(os.listdir(args.background)))
+
+
+image = Image.new("RGB", A4_size, color=(255,255,255))
+image = preprocessing.add_background(image, background_path, horizontal = 3)
+
+
+# create a draw object and select the font
+draw = ImageDraw.Draw(image)
+font_path = os.path.join(PATH, args.font, random.choice(os.listdir(args.font)))
+font = ImageFont.truetype(f"{font_path}", 36)
+
+text_boxes = []
+# generate the table with random characters from the dictionary
+for i in range(10):
+    for j in range(2):
+        x = 10 + j * 1050
+        y = 10 + i * 148
+        text = random.choice(dictionary)
+        # Draw text and save the text box
+        width, height = draw.textsize(text, font=font)
+        text_boxes.append((x, x + width, y, y + height))
+        draw.text((x, y), text, font=font, fill=args.color)
+        draw.rectangle((x, y, x + width, y + height), outline="red")
+
+# print(text_boxes)
+
+# save the image
+image.save("image.jpg")
---
+++ preprocessing.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageDraw
+
+def add_background(image, background_path, horizontal = 3, vertical = None):
+    background = Image.open(background_path)
+    background_width, background_height = background.size
+    ratio = background_width/background_height
+
+    width, height = image.size
+    
+    if vertical != None:
+        background_height = height//vertical + 1
+        background_width = int(background_height*ratio)
+    else:
+        background_width = width//horizontal + 1
+        background_height = int(background_width/ratio)
+
+    # Resize background, keep aspect ratio
+    background = background.resize((background_width, background_height))
+
+    # Concatenate the multiple background image and paste it to the image
+    for i in range(0, height, background_height):
+        for j in range(0, width, background_width):
+            image.paste(background, (j, i))
+
+    return image
+
+def add_text(image, font, text, text_size = 20, text_color = "white"):
+    draw = ImageDraw.Draw(image)
+
+    text_bboxes = []
+    
+
+
+                    
