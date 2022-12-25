c89695bd660772a6da6806c3876a76dff0acbeb2
revgato <locchoihay147@gmail.com>
2022-12-25 05:36:31 +0000

    Add more arguments

--- img_generate.py
+++ img_generate.py
@@ -1,4 +1,4 @@
-# python img_generate.py --font ../fonts/ko --background ../TextRecognitionDataGenerator/trdg/images/ --color black --dictionary dict.txt
+# python img_generate.py --font fonts --background images --color black --dictionary dict.txt
 import argparse
 import random
 import os
@@ -13,6 +13,7 @@ parser.add_argument("--font", required=True, help="folder containing font")
 parser.add_argument("--background", required=True, help="folder containing background image")
 parser.add_argument("--color", required=True, help="font color")
 parser.add_argument("--dictionary", required=True, help="dictionary file")
+parser.add_argument("--output", default="outputs", help="output folder")
 args = parser.parse_args()
 
 # read the dictionary file
@@ -25,6 +26,7 @@ PATH = os.getcwd()
 background_path = os.path.join(PATH, args.background)
 dictionary_path = os.path.join(PATH, args.dictionary)
 font_path = os.path.join(PATH, args.font)
+output_path = os.path.join(PATH, args.output)
 
 generator = Generator(size = A4_size, dictionary_path=dictionary_path, background_path=background_path, font_path=font_path)
 
@@ -34,9 +36,9 @@ image, text_boxes = generator.add_text(text_size = 30, text_color=args.color, n_
 # print(text_boxes)
 
 # save the image
-image.save("image.jpg")
+image.save(os.path.join(output_path, "image1.jpg"))
 
 image, text_boxes = generator.add_text(text_size = 100, text_color=args.color)
 
-image.save("image2.jpg")
+image.save(os.path.join(output_path, "image2.jpg"))
 
